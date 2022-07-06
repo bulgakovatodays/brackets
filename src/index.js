8fc0484ccb8b02acbd17a785c1ca04112954689f
bulgakovatodays <lesyataranina@gmail.com>
2022-07-06 20:05:37 +0300

    init: done task!))

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+ // console.log('Length of string is ' + str.length);
+ // console.log('Length of argument is ' + config.length);
+
+  function checkString() {
+  for (let i = 0; i < str.length; i++){
+ //   console.log('Interation #' + (i + 1));
+    let check = config[i];
+ //   console.log('Word for check ' + check);
+    checkStr = String(check).replace(',', '');
+ //   console.log('StringWord is ' + checkStr);
+ //   console.log(str.length);
+ //   console.log(str.includes(checkStr));
+    str = str.replace(checkStr, '');
+ //   console.log('New string is "' + str + '"');
+    }
+  }
+    for (let i = 0; i < str.length; i++){
+      //console.log('Interation #' + (i + 1));
+      checkString()
+      checkString()
+    }
+   
+    for (let i = 0; i < str.length; i++){
+      //console.log('Check: ' + (i + 1));
+      checkString()
+      checkString()
+    }
+
+ 
+  if (str === '') {
+    return(true);
+  } else {
+    return(false);
+  }
 }
